f6891b6b37b74532a4ce9df4cb6a16ac2a130cda
“Mavene1” <maveneomondi111@gmail.com>
2024-07-11 11:38:23 +0300

    first commit

---
+++ src/main/java/com/mavene/jwt_security/JwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mavene.jwt_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityApplication.class, args);
+		System.out.println("Maven JWT Security Running...");
+	}
+}
